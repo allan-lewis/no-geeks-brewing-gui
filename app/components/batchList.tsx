b4791fa54befda4bb831ab5ea40c183b55d08328
allan <allan.e.lewis@gmail.com>
2024-06-15 14:22:31 -0400

    #1 - Basic Layout

--- app/components/batchList.tsx
+++ app/components/batchList.tsx
@@ -15,13 +15,13 @@ function loadPage(status: string) {
   const showError = false;
 
   return (
-    <div>
+    <div className="ngb-container">
       {error || showError ? (
         <ErrorPlaceholder></ErrorPlaceholder>
       ) : showLoading || isLoading || isFetching ? (
         <LoadingPlaceholder></LoadingPlaceholder>
       ): data ? (
-        <div className="bg-pink-400 grid lg:w-full lg:mb-0 lg:grid-cols-3 flex gap-4">
+        <div className="bg-pink-400 grid lg:w-full lg:mb-0 lg:grid-cols-3 flex gap-4 px-4 py-4">
           {data.map((batch) => (
             <RecipeCard key={batch.id} batch={batch}></RecipeCard>
           ))}
@@ -33,27 +33,26 @@ function loadPage(status: string) {
 
 function ErrorPlaceholder() {
   return (
-      <div className="relative flex justify-center fill-space h-full w-full bg-violet-400 items-center h-auto w-auto flex flex-col items-center justify-between p-24">
-          <h1 className="text-rose-700 mb-4 mt-4 flex flex items-center text-xl font-semibold bg-gray-400 h-full">Something went wrong, please try again</h1>
+      <div>
+        <h1 className="text-rose-700 bg-gray-400 flex flex-col align-middle items-center text-xl font-semibold">Something went wrong, please try again</h1>
       </div>
   )
 }
 
 function LoadingPlaceholder() {
   return (
-      <div className="relative flex justify-center fill-space h-full w-full bg-violet-400 items-center h-auto w-auto flex flex-col items-center justify-between p-24">
-          <h1 className="mb-4 mt-4 flex flex items-center text-xl font-semibold bg-gray-400 h-full">Just one moment...</h1>
+      <div>
+          <h1 className="bg-gray-400 flex flex-col align-middle items-center text-xl font-semibold">Working on things, wait one...</h1>
       </div>
   )
 }
 
 function RecipeCard({batch} : {batch: Batch}) {
   return (
-      <div className="block  p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 lg:w-full">
+      <div className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 lg:w-full">
           <h1 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{batch.name}</h1>
           <h2 className="mb-2 text-l font-bold tracking-tight text-gray-600 dark:text-white">{batch.style}</h2>
           <h2 className="mb-2 text-s tracking-tight text-gray-600 dark:text-white">Batch #{batch.batchNumber}</h2>
       </div>
   )
 }
-
